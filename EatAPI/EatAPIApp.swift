7e197c1cb318fd34f2a4b5eb20d8709ea5f5eda5
jotkafomat <k-j@xl.wp.pl>
2021-03-10 13:02:26 +0000

    Initial Commit

---
+++ EatAPI/EatAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatAPIApp.swift
+//  EatAPI
+//
+//  Created by Krzysztof Jankowski on 10/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct EatAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
